5182995f644c80e048fdb1ebc31cea3a2f3d45b1
Vasco Carvalho <vascocarvalho2001@gmail.com>
2026-03-12 03:15:56 +0000

    Added 2 functions, one that collects data from a json for champions names and another one for items, and made a randomizer to choose a random champions and 5 items

--- main.py
+++ main.py
@@ -1,8 +1,35 @@
 import requests
+import random
 
-a = requests.get("https://ddragon.leagueoflegends.com/cdn/12.11.1/data/en_US/champion.json")
 
-data = a.json()
+def get_name():
+        all_champs = []
 
-for champ in data["data"]:
-        print(data["data"][champ]["name"])
+        get_champs = requests.get("https://ddragon.leagueoflegends.com/cdn/12.11.1/data/en_US/champion.json")
+
+        data_names = get_champs.json()
+
+        for champ in data_names["data"]:
+                all_champs.append(data_names["data"][champ]["name"])
+
+        print("Champion: ", all_champs[random.randrange(0,len(all_champs))])
+
+def get_items():
+        all_items = []
+
+        get_items = requests.get("https://ddragon.leagueoflegends.com/cdn/16.5.1/data/en_US/item.json")
+
+        data_items = get_items.json()
+
+        #print(data_items)
+
+
+        for item in data_items["data"]:
+                if data_items["data"][item]["gold"]["purchasable"] and data_items["data"][item]["maps"]["11"] and "into" not in data_items["data"][item] and "from" in data_items["data"][item]:
+                        all_items.append(data_items["data"][item]["name"])
+
+        for x in range(1, 6):
+                print(f"{x} item: ", all_items[random.randrange(0,len(all_items))])
+
+get_name()
+get_items()
