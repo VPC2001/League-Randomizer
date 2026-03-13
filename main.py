c72f2b06bfbfe8a7bd078dcfe32a87c826aff1f4
Vasco Carvalho <vascocarvalho2001@gmail.com>
2026-03-13 22:43:45 +0000

    Added a new functions for spells, it gets the JSON and retrieves the name of the spells and random picks 2 spells

--- main.py
+++ main.py
@@ -21,9 +21,6 @@ def get_items():
 
         data_items = get_items.json()
 
-        #print(data_items)
-
-
         for item in data_items["data"]:
                 if data_items["data"][item]["gold"]["purchasable"] and data_items["data"][item]["maps"]["11"] and "into" not in data_items["data"][item] and "from" in data_items["data"][item]:
                         all_items.append(data_items["data"][item]["name"])
@@ -31,5 +28,28 @@ def get_items():
         for x in range(1, 6):
                 print(f"{x} item: ", all_items[random.randrange(0,len(all_items))])
 
+
+def get_spells():
+        all_spells = []
+
+        get_spells = requests.get("https://ddragon.leagueoflegends.com/cdn/16.5.1/data/en_US/summoner.json")
+        
+        data_spells = get_spells.json()
+
+        for spell in data_spells["data"]:
+                for x in data_spells["data"][spell]["modes"]:
+                        if x == "CLASSIC":
+                                all_spells.append(data_spells["data"][spell]["name"])
+
+        rand_spell = random.sample(all_spells, 2)
+
+        for x in range(0, len(rand_spell)):
+                suffix = "st" if x == 1 else "nd"
+                print(f"{x+1}{suffix} spell: {rand_spell[x]}")
+                
+
 get_name()
+print("\n")
+get_spells()
+print("\n")
 get_items()
