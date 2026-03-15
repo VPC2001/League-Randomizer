dfa74f7e149d60af1bf39ac2a33d5251bda28a20
Vasco Carvalho <vascocarvalho2001@gmail.com>
2026-03-15 17:07:27 +0000

    Got all the data I need from the Api, added function to get all the runes needed and reworked a bit the function to get all the names from champions to also get the ability to max

--- main.py
+++ main.py
@@ -12,7 +12,22 @@ def get_name():
         for champ in data_names["data"]:
                 all_champs.append(data_names["data"][champ]["name"])
 
-        print("Champion:", all_champs[random.randrange(0,len(all_champs))])
+        champ_name = all_champs[random.randrange(0,len(all_champs))]
+
+        url_champs_stats = requests.get(f"https://ddragon.leagueoflegends.com/cdn/16.5.1/data/en_US/champion/{champ_name}.json")
+
+        champ_stats_data = url_champs_stats.json()
+
+        print(f"Champion: {champ_name}")
+        
+        index = random.randrange(0, 3)
+
+        random_ability = champ_stats_data["data"][champ_name]["spells"][index]["name"]
+
+        abilities = ["Q", "W", "E"]
+
+        print(f"\nAbility to max: {abilities[index]} ({random_ability})")
+
 
 def get_items():
         all_items = []
@@ -63,25 +78,49 @@ def get_runes():
 
         data_runes = get_runes.json()
 
-        rune_head = random.randrange(0,4)
-
+        rune_path = random.sample(list(range(0, len(data_runes))), 2)
+ 
         # Used this to understand how to pick things from the list 
         # for x in range(len(data_runes)):
         #         print(data_runes[x]["name"])
         #         for y in range(len(data_runes[x]["slots"])):
         #                 for z in range(len(data_runes[x]["slots"][y]["runes"])):
         #                         print(data_runes[x]["slots"][y]["runes"][z]["name"])
-                                
 
-        print(data_runes[rune_head]["name"])
+        # Primary Rune Path
+        print("Runes 1st part: ")
+        print(data_runes[rune_path[0]]["name"])
         for i in range(0, 4):
-                print(data_runes[rune_head]["slots"][i]["runes"][random.randrange(0,3)]["name"])
+                print(data_runes[rune_path[0]]["slots"][i]["runes"][random.randrange(0,3)]["name"])
 
-# get_name()
-# print()
-# get_roles()
-# print()
-# get_spells()
-# print()
-# get_items()
-get_runes()
+        # Secondary Rune Path
+        minor_runes = random.sample(range(1,4), 2)
+        print("\nRunes 2nd part: ")
+        print(data_runes[rune_path[1]]["name"])
+        for x in minor_runes:
+                print(data_runes[rune_path[1]]["slots"][x]["runes"][random.randrange(0,3)]["name"])
+
+        # Stats Shards
+        stat_shards = [
+                ["Adaptive Force", "Attack Speed", "Ability Haste"],
+                ["Adaptive Force", "Move Speed", "Health Scaling"],
+                ["Scaling Health", "Tenacity and Slow Resist", "Health Scaling"]
+        ]
+
+        print("\nStats Shards: ")
+        for y in range(len(stat_shards)):
+                print(stat_shards[y][random.randrange(0, 3)])
+
+
+
+
+for _ in range(1):
+        get_name()
+        print()
+        get_roles()
+        print()
+        get_spells()
+        print()
+        get_items()
+        print()
+        get_runes()
