b8fa0ce47195bc5c69afedc5ea8157256183b592
Vasco Carvalho <vascocarvalho2001@gmail.com>
2026-03-19 03:44:08 +0000

    Added error catch for the API

--- api.py
+++ api.py
@@ -1,8 +1,11 @@
-from flask import Flask
+from flask import Flask, jsonify
 from main import gen_build
 
 app = Flask(__name__)
 
 @app.route("/get_data")
 def get_data():
-    return gen_build()
+    try:
+        return jsonify(gen_build())
+    except Exception as e:
+        return jsonify({"error ": str(e)}), 500
